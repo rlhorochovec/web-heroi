17db5bae019a81a3aa0675c2834b0f954c911353
Rafael Horochovec <rl.horochovec@gmail.com>
2022-01-25 17:58:56 -0300

    Rotas e navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarHeroisComponent } from './components/listar-herois/listar-herois.component';
+import { DetalharHeroiComponent } from './components/detalhar-heroi/detalhar-heroi.component';
+import { CriarHeroiComponent } from './components/criar-heroi/criar-heroi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'herois', pathMatch: 'full' },
+  { path: 'herois', component: ListarHeroisComponent },
+  { path: 'herois/:id', component: DetalharHeroiComponent },
+  { path: 'criar', component: CriarHeroiComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,9 @@ import { ListarHeroisComponent } from './components/listar-herois/listar-herois.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
