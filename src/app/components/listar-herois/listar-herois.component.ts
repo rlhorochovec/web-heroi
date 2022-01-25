85ad4be665cb04cae65c52efa0dfab8e6ea4c3dd
Rafael Horochovec <rl.horochovec@gmail.com>
2022-01-25 18:50:05 -0300

    Listar registros

--- src/app/components/listar-herois/listar-herois.component.ts
+++ src/app/components/listar-herois/listar-herois.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroi } from 'src/app/models/heroi.model';
+import { HeroiService } from 'src/app/services/heroi.service';
 
 @Component({
   selector: 'app-listar-herois',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarHeroisComponent implements OnInit {
 
-  constructor() { }
+  herois?: Heroi[];
+  currentHeroi: Heroi = {};
+  currentIndex = -1;
+  nome = '';
+
+  constructor(private heroiService: HeroiService) { }
 
   ngOnInit(): void {
+    this.listar();
   }
 
+  listar(): void {
+    this.heroiService.list()
+      .subscribe(
+        data => {
+          this.herois = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  recarregar(): void {
+    this.listar();
+    this.currentHeroi = {};
+    this.currentIndex = -1;
+  }
+
+  selecionar(heroi: Heroi, index: number): void {
+    this.currentHeroi = heroi;
+    this.currentIndex = index;
+  }
+
+  limpar(): void {
+    this.heroiService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.recarregar();
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  pesquisar(): void {
+    this.currentHeroi = {};
+    this.currentIndex = -1;
+
+    this.heroiService.findByName(this.nome)
+      .subscribe(
+        data => {
+          this.herois = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
